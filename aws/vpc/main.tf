4c30073ff58bc8ac6388827e19dcef7de7d7b354
bruninrlx <bruno.lendro2000@gmail.com>
2023-06-25 16:17:26 -0300

    feat: create vpc

---
+++ aws/vpc/main.tf
@@ -0,0 +1,29 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  name                 = "my-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/my-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-cluster" = "shared"
+    "kubernetes.io/role/elb"           = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"  = "1"
+  }
+}
---
+++ aws/vpc/outputs.tf
@@ -0,0 +1,24 @@
+output "azs" {
+  description = "Availability Zones"
+  value   = module.vpc.azs
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value   = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet ids"
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet ids"
+  value = module.vpc.public_subnets
+}
+
+output "intra_subnets" {
+  description = "Intra subnet ids"
+  value = module.vpc.intra_subnets
+}
